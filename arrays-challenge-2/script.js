18d5afc5ac8df3434043c88551fbaf5229b84e1a
santanagabi <gaboiveirasantana1@gmail.com>
2023-02-13 21:43:49 -0300

    Calculate the averagee human age of all adult dogs

--- arrays-challenge-2/script.js
+++ arrays-challenge-2/script.js
@@ -91,3 +91,17 @@
 // Test data:
 // § Data 1: [5, 2, 4, 1, 15, 8, 3]
 // § Data 2: [16, 6, 10, 5, 6, 1, 4]
+
+const calcAverageHumanAge = function (ages) {
+  const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+
+  const adultDogs = humanAges.filter(age => age >= 18);
+
+  // Average
+  const average = adultDogs.reduce((acc, cur) => {
+    acc + cur / adultDogs.length;
+  }, 0);
+  console.log(average);
+};
+
+calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
