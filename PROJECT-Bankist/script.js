72f6888e7cf5a072a038e53b1cdd3dbf0074fb46
santanagabi <gaboiveirasantana1@gmail.com>
2023-03-04 22:04:54 -0300

    Learning about creating and filling arrays

--- PROJECT-Bankist/script.js
+++ PROJECT-Bankist/script.js
@@ -284,30 +284,74 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // Sorting arrays
 
 // Strings
-const owners = ['Jonas', 'Zach', 'Addam', 'Marta'];
-console.log(owners.sort());
-console.log(owners); // change the original array
+// const owners = ['Jonas', 'Zach', 'Addam', 'Marta'];
+// console.log(owners.sort());
+// console.log(owners); // change the original array
 
-// Numbers
-console.log(movements);
-// console.log(movements.sort()); // sort by strings
+// // Numbers
+// console.log(movements);
+// // console.log(movements.sort()); // sort by strings
 
-// return < 0, A, B (keeep order)
-// return > 0, B, A (switch order)
+// // return < 0, A, B (keeep order)
+// // return > 0, B, A (switch order)
 
-// ascender
-// movements.sort((a, b) => {
-//   if (a > b) return 1;
-//   if (a < b) return -1;
-// });
-movements.sort((a, b) => a - b);
-// if a > b = positive number
-console.log(movements);
+// // ascender
+// // movements.sort((a, b) => {
+// //   if (a > b) return 1;
+// //   if (a < b) return -1;
+// // });
+// movements.sort((a, b) => a - b);
+// // if a > b = positive number
+// console.log(movements);
 
-// descending
-// movements.sort((a, b) => {
-//   if (a > b) return -1;
-//   if (a < b) return 1;
-// });
-movements.sort((a, b) => b - a); // return b - a
-console.log(movements);
+// // descending
+// // movements.sort((a, b) => {
+// //   if (a > b) return -1;
+// //   if (a < b) return 1;
+// // });
+// movements.sort((a, b) => b - a); // return b - a
+// console.log(movements);
+
+///////////////////////////////////////////////////////////////
+// Creating and filling arrays
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+// empty arrays + fill method
+const x = new Array(7); // new empty array
+console.log(x);
+// console.log(x.map(() => 5));
+
+x.fill(1);
+x.fill(1, 3, 5); // 1 strats in index 3 // end in 5
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1); // map function = () => 1
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+// 100 random dices
+const randomDice = Array.from({ length: 100 }, () =>
+  Math.floor(Math.random() * 6)
+);
+console.log(randomDice);
+
+// arrays movements IU
+
+labelBalance.addEventListener('click', function () {
+  // Array.from() = create a array => querySelectorAll
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', '')) // map
+  );
+  console.log(movementsUI);
+
+  movementsUI2 = [...document.querySelectorAll('.movements__value')];
+});
