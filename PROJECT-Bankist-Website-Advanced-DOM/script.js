2abe4370ae3a6e04b2af4b5268f7038aed38fd4d
santanagabi <gaboliveirasantana1@gmail.com>
2023-06-13 21:13:28 -0300

    learning about event propagation - bubbling

--- PROJECT-Bankist-Website-Advanced-DOM/script.js
+++ PROJECT-Bankist-Website-Advanced-DOM/script.js
@@ -177,14 +177,20 @@ const randomColor = () =>
 console.log(randomColor(0, 255));
 
 document.querySelector('.nav__link').addEventListener('click', function (e) {
-  this.style.backgroundColor = randomColor()
-  console.log('link');
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+  // Stop propagation
+  // e.stopPropagation();
 });
 
 document.querySelector('.nav__links').addEventListener('click', function (e) {
-  console.log('link');
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
 });
 
 document.querySelector('.nav').addEventListener('click', function (e) {
-  console.log('link');
+  this.style.backgroundColor = randomColor();
+  console.log('NAV', e.target, e.currentTarget);
 });
